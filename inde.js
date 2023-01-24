b0b61b3b2606159fa24cbf0e02792baf5f0ac0b2
githukelvin <kelvingithu019@gmail.com>
2023-01-24 09:46:21 +0300

    first commit

--- inde.js
+++ inde.js
@@ -39,3 +39,49 @@ const tog = document.querySelector(".toggle");
 tog.addEventListener("click", () => {
   document.body.classList.toggle("dark-background");
 });
+
+ function show(anything) {
+   document.querySelector(".box").value = anything;
+   const url =`https://restcountries.com/v3.1/region/${anything}`;
+  //  console.log(url);
+   
+     const bdiz = document.querySelector(".flags");
+    
+       const response = fetch(url);
+       response.then(data => {return data.json()})
+       .then(data =>{
+          
+       bdiz.innerHTML = data
+         .map(
+           ({
+             name: { common },
+             population,
+             continents,
+             capital,
+             flags: { png },
+           }) => {
+             return `
+        <div class="flag">
+          <div class="img">
+            <img src="${png}" alt="${common} flag">
+          </div>
+          <div class="details">
+            <h2>${common}</h2>
+            <h3>Population: <small>${population.toLocaleString()}</small></h3>
+            <h3>Region: <small>${continents[0]}</small></h3>
+            <h3>Capital: <small>${capital}</small></h3>
+          </div>
+        </div>
+      `;
+           }
+         )
+         .join("");
+       }
+         
+       )
+      
+   }
+ let dp = document.querySelector(".custom-select");
+ dp.onclick = () => {
+   dp.classList.toggle("active");
+ };
